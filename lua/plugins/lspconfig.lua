07b65a719889682afff427128ceb33c808f0e390
fill1890 <andrew.walls2@gmail.com>
2025-03-03 18:59:40 +1100

    various changes, new completion

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,86 +6,45 @@ end
 
 return {
     'neovim/nvim-lspconfig',
-    build = ':COQdeps',
-    config = function()
+
+    opts = {
+        servers = {
+            clangd = {},
+            basedpyright = {},
+            ltex = {
+                settings = {ltex = {
+                    language = 'en-AU',
+                    enabled = {
+                        'latex', 'markdown', 'restructuredtext',
+                        'python',
+                    }
+                }},
+                filetypes = {
+                    'bib', 'gitcommit', 'markdown', 'org', 'plaintex',
+                    'tex', 'rst', 'pandoc', 'lua',
+                },
+            }
+        },
+    },
+
+    config = function(_, opts)
         require('mason').setup()
         require('mason-lspconfig').setup()
         local lsp = require'lspconfig'
-        local c_prep = require'coq'.lsp_ensure_capabilities
-
-        lsp.arduino_language_server.setup(c_prep{
-            cmd = {
-                "arduino-language-server",
-                "-cli-config", "/Users/andrew/Library/Arduino15/arduino-cli.yaml",
-                "-fqbn", "arduino:avr:uno",
-                "-cli", "arduino-cli",
-                "-clangd", "clangd"
-            },
-            on_attach = keybinds
-        })
-
-        lsp.clangd.setup(c_prep{ on_attach = keybinds })
-
-        lsp.pyright.setup(c_prep{ on_attach = keybinds })
-
-        lsp.ts_ls.setup(c_prep{ on_attach = keybinds })
-
-        lsp.jdtls.setup(c_prep{ on_attach = keybinds })
-
-        lsp.spectral.setup(c_prep{ on_attach = keybinds })
-
-        lsp.ltex.setup(c_prep{
-            settings = {ltex = {
-                language = 'en-AU',
-                enabled = {
-                    'latex', 'markdown', 'restructuredtext',
-                    'python',
-                }
-            }},
-            filetypes = {
-                'bib', 'gitcommit', 'markdown', 'org', 'plaintex',
-                'tex', 'rst', 'pandoc', 'lua',
-            },
-
-            on_attach = keybinds
-        })
-
-        lsp.lua_ls.setup(c_prep{
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = 'LuaJIT'
-                    },
-                    diagnostics = {
-                        globals = { 'vim' }
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true)
-                    },
-                    telemetry = {
-                        enable = false
-                    }
-                }
-            },
-
-            on_attach = keybinds
-        })
+        for server, config in pairs(opts.servers) do
+            -- passing config.capabilities to blink.cmp merges with the capabilities in your
+            -- `opts[server].capabilities, if you've defined it
+            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            lsp[server].setup(config)
+        end
     end,
 
     dependencies = {
-        { 'ms-jpq/coq.nvim', branch = 'coq' },
-        { 'ms-jpq/coq.artifacts', branch='artifacts' },
+        { 'saghen/blink.cmp' },
         { 'williamboman/mason-lspconfig.nvim' }
     },
 
     init = function()
-        vim.g.coq_settings = {
-            auto_start = 'shut-up',
-            keymap = { recommended = false },
-            clients = {
-                tree_sitter = { weight_adjust = 1 }
-            },
-        }
     end,
 
     lazy = false,
